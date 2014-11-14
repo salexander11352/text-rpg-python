f526760ce05c1216241a2c4a19be76d6630ec78a
nyaccino <nyaccino@gmail.com>
2014-11-13 20:13:03 -0600

    Move classAttr functions to separate file
    Also hotfix for infinite loop in character creation code

---
+++ classAttr.py
@@ -0,0 +1,100 @@
+
+playerAttr = {}
+
+# Character Attr Functions
+def warriorAttr():
+    playerAttr["role"] = "Warrior"
+    playerAttr["pos"] = [0,0]
+    playerAttr["stat"] = {}
+    playerAttr["stat"]["HP"] = 0
+    playerAttr["stat"]["vit"] = 14
+    playerAttr["stat"]["int"] = 9
+    playerAttr["stat"]["end"] = 12
+    playerAttr["stat"]["str"] = 12
+    playerAttr["stat"]["dex"] = 11
+    playerAttr["stat"]["mag"] = 8
+    playerAttr["stat"]["fth"] = 10
+    playerAttr["stat"]["lck"] = 7
+    playerAttr["stat"]["wpn"] = 40
+    playerAttr["stat"]["arm"] = 35
+
+def knightAttr():
+    playerAttr["role"] = "Knight"
+    playerAttr["pos"] = [0,0]
+    playerAttr["stat"] = {}
+    playerAttr["stat"]["HP"] = 0
+    playerAttr["stat"]["vit"] = 10
+    playerAttr["stat"]["int"] = 11
+    playerAttr["stat"]["end"] = 11
+    playerAttr["stat"]["str"] = 14
+    playerAttr["stat"]["dex"] = 10
+    playerAttr["stat"]["mag"] = 10
+    playerAttr["stat"]["fth"] = 11
+    playerAttr["stat"]["lck"] = 10
+    playerAttr["stat"]["wpn"] = 35
+    playerAttr["stat"]["arm"] = 40
+
+def royaltyAttr():
+    playerAttr["role"] = "Royalty"
+    playerAttr["pos"] = [0,0]
+    playerAttr["stat"] = {}
+    playerAttr["stat"]["HP"] = 0
+    playerAttr["stat"]["vit"] = 8
+    playerAttr["stat"]["int"] = 12
+    playerAttr["stat"]["end"] = 8
+    playerAttr["stat"]["str"] = 9
+    playerAttr["stat"]["dex"] = 12
+    playerAttr["stat"]["mag"] = 13
+    playerAttr["stat"]["fth"] = 12
+    playerAttr["stat"]["lck"] = 7
+    playerAttr["stat"]["wpn"] = 30
+    playerAttr["stat"]["arm"] = 15
+
+def mageAttr():
+    playerAttr["role"] = "Mage"
+    playerAttr["pos"] = [0,0]
+    playerAttr["stat"] = {}
+    playerAttr["stat"]["HP"] = 0
+    playerAttr["stat"]["vit"] = 9
+    playerAttr["stat"]["int"] = 15
+    playerAttr["stat"]["end"] = 10
+    playerAttr["stat"]["str"] = 9
+    playerAttr["stat"]["dex"] = 11
+    playerAttr["stat"]["mag"] = 15
+    playerAttr["stat"]["fth"] = 6
+    playerAttr["stat"]["lck"] = 11
+    playerAttr["stat"]["wpn"] = 32
+    playerAttr["stat"]["arm"] = 20
+
+def thiefAttr():
+    playerAttr["role"] = "Thief"
+    playerAttr["pos"] = [0,0]
+    playerAttr["stat"] = {}
+    playerAttr["stat"]["HP"] = 0
+    playerAttr["stat"]["vit"] = 10
+    playerAttr["stat"]["int"] = 13
+    playerAttr["stat"]["end"] = 10
+    playerAttr["stat"]["str"] = 9
+    playerAttr["stat"]["dex"] = 14
+    playerAttr["stat"]["mag"] = 10
+    playerAttr["stat"]["fth"] = 8
+    playerAttr["stat"]["lck"] = 15
+    playerAttr["stat"]["wpn"] = 20
+    playerAttr["stat"]["arm"] = 25
+
+def hunterAttr():
+    playerAttr["role"] = "Hunter"
+    playerAttr["pos"] = [0,0]
+    playerAttr["stat"] = {}
+    playerAttr["stat"]["HP"] = 0
+    playerAttr["stat"]["vit"] = 12
+    playerAttr["stat"]["int"] = 10
+    playerAttr["stat"]["end"] = 13
+    playerAttr["stat"]["str"] = 11
+    playerAttr["stat"]["dex"] = 12
+    playerAttr["stat"]["mag"] = 8
+    playerAttr["stat"]["fth"] = 8
+    playerAttr["stat"]["lck"] = 12
+    playerAttr["stat"]["wpn"] = 45
+    playerAttr["stat"]["arm"] = 15
+
--- game.py
+++ game.py
@@ -3,8 +3,8 @@ import platform
 import random
 import sys
 import time
-
 import console as con
+from classAttr import *
 
 con.set_text_color(con.darkgreen, con.black)
 
@@ -67,6 +67,7 @@ terMap = [[M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, M, 
 
 # Constants
 DEBUG = False
+running = True
 
 # Battle Constants
 HP_BASE_MOD  = 50
@@ -77,11 +78,6 @@ MISS_MOD     = 1
 REROLL_MOD   = .85
 WPN_STAT_MOD = 800.0
 
-# Player information
-playerAttr = {}
-
-running = True
-
 # Enemy Attr
 enemy = {}
 enemy["vit"] = 10
@@ -90,106 +86,7 @@ enemy["lck"] = 9
 enemy["wpn"] = 0
 enemy["arm"] = 0
 
-# Character Attr Functions
-#warriorAttr["stat"]["lck"]
-def warriorAttr():
-    playerAttr["role"] = "Warrior"
-    playerAttr["pos"] = [0,0]
-    playerAttr["stat"] = {}
-    playerAttr["stat"]["HP"] = 0
-    playerAttr["stat"]["vit"] = 14
-    playerAttr["stat"]["int"] = 9
-    playerAttr["stat"]["end"] = 12
-    playerAttr["stat"]["str"] = 12
-    playerAttr["stat"]["dex"] = 11
-    playerAttr["stat"]["mag"] = 8
-    playerAttr["stat"]["fth"] = 10
-    playerAttr["stat"]["lck"] = 7
-    playerAttr["stat"]["wpn"] = 40
-    playerAttr["stat"]["arm"] = 35
-
-def knightAttr():
-    playerAttr["role"] = "Knight"
-    playerAttr["pos"] = [0,0]
-    playerAttr["stat"] = {}
-    playerAttr["stat"]["HP"] = 0
-    playerAttr["stat"]["vit"] = 10
-    playerAttr["stat"]["int"] = 11
-    playerAttr["stat"]["end"] = 11
-    playerAttr["stat"]["str"] = 14
-    playerAttr["stat"]["dex"] = 10
-    playerAttr["stat"]["mag"] = 10
-    playerAttr["stat"]["fth"] = 11
-    playerAttr["stat"]["lck"] = 10
-    playerAttr["stat"]["wpn"] = 35
-    playerAttr["stat"]["arm"] = 40
-
-def royaltyAttr():
-    playerAttr["role"] = "Royalty"
-    playerAttr["pos"] = [0,0]
-    playerAttr["stat"] = {}
-    playerAttr["stat"]["HP"] = 0
-    playerAttr["stat"]["vit"] = 8
-    playerAttr["stat"]["int"] = 12
-    playerAttr["stat"]["end"] = 8
-    playerAttr["stat"]["str"] = 9
-    playerAttr["stat"]["dex"] = 12
-    playerAttr["stat"]["mag"] = 13
-    playerAttr["stat"]["fth"] = 12
-    playerAttr["stat"]["lck"] = 7
-    playerAttr["stat"]["wpn"] = 30
-    playerAttr["stat"]["arm"] = 15
-
-def mageAttr():
-    playerAttr["role"] = "Mage"
-    playerAttr["pos"] = [0,0]
-    playerAttr["stat"] = {}
-    playerAttr["stat"]["HP"] = 0
-    playerAttr["stat"]["vit"] = 9
-    playerAttr["stat"]["int"] = 15
-    playerAttr["stat"]["end"] = 10
-    playerAttr["stat"]["str"] = 9
-    playerAttr["stat"]["dex"] = 11
-    playerAttr["stat"]["mag"] = 15
-    playerAttr["stat"]["fth"] = 6
-    playerAttr["stat"]["lck"] = 11
-    playerAttr["stat"]["wpn"] = 32
-    playerAttr["stat"]["arm"] = 20
-
-def thiefAttr():
-    playerAttr["role"] = "thief"
-    playerAttr["pos"] = [0,0]
-    playerAttr["stat"] = {}
-    playerAttr["stat"]["HP"] = 0
-    playerAttr["stat"]["vit"] = 10
-    playerAttr["stat"]["int"] = 13
-    playerAttr["stat"]["end"] = 10
-    playerAttr["stat"]["str"] = 9
-    playerAttr["stat"]["dex"] = 14
-    playerAttr["stat"]["mag"] = 10
-    playerAttr["stat"]["fth"] = 8
-    playerAttr["stat"]["lck"] = 15
-    playerAttr["stat"]["wpn"] = 20
-    playerAttr["stat"]["arm"] = 25
-
-def hunterAttr():
-    playerAttr["role"] = "hunter"
-    playerAttr["pos"] = [0,0]
-    playerAttr["stat"] = {}
-    playerAttr["stat"]["HP"] = 0
-    playerAttr["stat"]["vit"] = 12
-    playerAttr["stat"]["int"] = 10
-    playerAttr["stat"]["end"] = 13
-    playerAttr["stat"]["str"] = 11
-    playerAttr["stat"]["dex"] = 12
-    playerAttr["stat"]["mag"] = 8
-    playerAttr["stat"]["fth"] = 8
-    playerAttr["stat"]["lck"] = 12
-    playerAttr["stat"]["wpn"] = 45
-    playerAttr["stat"]["arm"] = 15
-
 #  Game Functions
-
 def debug(s):
     if DEBUG:
         print ('[DEBUG]: %s' % s)
@@ -390,8 +287,8 @@ def skillAlc():
               "3", "4",
               "5", "6",
               "7", "8")
-
-    while True:
+    charMenu = True
+    while charMenu:
         print ("""
         Character Name: %s\n
         Vitality ...... %d
@@ -409,7 +306,7 @@ def skillAlc():
                 1. Name Your Character
                 2. Spend Skill Points
                 3. Remove Skill Points
-                4. Exit Character Creation
+                4. Start your adventure
                 \n\n""")
         choice = raw_input(" >>> ")
         choosingName = True
@@ -544,14 +441,14 @@ def skillAlc():
                     print " ERROR: Please inout a attribute."
                     raw_input(" Press Enter to go back to menu.")
             elif choice == "4":
-                start = raw_input(" Do you want to start your adventure?\n >>> ")
-                if start.lower() == "yes" or start.lower() == "y":
-                    print" Adventure starting in 3 secs."
-                    time.sleep(1)
-                    print" Adventure starting in 2 secs."
-                    time.sleep(1)
-                    print" Adventure starting in 1 sec."
-                    time.sleep(1)
+                print" Adventure starting in 3 secs."
+                time.sleep(1)
+                print" Adventure starting in 2 secs."
+                time.sleep(1)
+                print" Adventure starting in 1 sec."
+                time.sleep(1)
+                choosingName = False
+                charMenu = False
 
     playerAttr["stat"]["vit"] = vitality    
     playerAttr["stat"]["end"] = endurance   
